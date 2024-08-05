edcc9a7d673d103f848f3af031101ebc2ae61157
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-08-05 11:22:46 -0300

    msg

--- corretora.js
+++ corretora.js
@@ -1,3 +1,26 @@
+// Utilizando agora IDs, faça o gerenciamento das seguintes entidades:
+
+// Corretora:
+//  nome
+
+// Corretor:
+//  nome
+//  id_corretora: relacionar com corretora
+
+// Imóvel
+//  numero
+//  rua
+//  bairro
+// id_corretora: relacionar com corretora
+
+// Cliente:
+//  nome
+
+// Venda
+//  id_imovel: relacionar com imóvel
+//  id_cliente: relacionar com cliente
+//  id_corretor: relacionar com corretor
+
 const prompt = require("prompt-sync")();
 
 let nextUserId = 1;
